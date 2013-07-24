1450ac244e3bb3a1b96de661a922958b14a18357
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-07-24 20:26:19 +1000

    Fix plugin-free Bespoke.js call

--- app/templates/src/js/js.js
+++ app/templates/src/js/js.js
@@ -1,4 +1,4 @@
-bespoke.horizontal.from('article'<% if (!bullets && !hash) { %>});<% } else { %>, {
+bespoke.horizontal.from('article'<% if (!bullets && !hash) { %>);<% } else { %>, {
 <% if (bullets) { %>  bullets: 'li'<% if (hash) { %>,
 <% } %><% } %><% if (hash) { %>  hash: true
 <% } %>});<% } %>
