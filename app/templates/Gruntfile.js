8c5c95051c8a51bffa5a3933e771abdb58fa72cc
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-07-26 08:14:53 +1000

    Fix matcher for copy task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'src/',
-          src: ['**', '!**/*.jade', '!**/*.styl'],
+          src: ['**/*', '!**/*.jade', '!**/*.styl'],
           dest: 'public/'
         }]
       }
