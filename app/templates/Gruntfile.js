9e60eb1a369a1dc534abccb00615faccd3bb6497
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-09-20 08:12:48 +1000

    Make preview server publicly accessible

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -93,6 +93,7 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 8000,
+          hostname: '*', // Remove this line if you only want the server available locally
           base: 'public',
           keepalive: true,
           middleware: function(connect, options) {
