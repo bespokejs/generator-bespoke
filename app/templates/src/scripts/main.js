9e4d58eba6a803818a2ca0c742fd4c55dae7ee02
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-05-30 17:35:47 +0200

    style: add EJS newline slurping to ease template readability

--- app/templates/src/scripts/main.js
+++ app/templates/src/scripts/main.js
@@ -1,17 +1,18 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke');
 <%- selectedPlugins.map(function (plugin) {
-    return 'var ' + plugin.varName + " = require('bespoke-" + plugin.name + "');";
+  return 'var ' + plugin.varName + " = require('bespoke-" + plugin.name + "');";
 }).join('\n'); %>
 
 // Bespoke.js
 bespoke.from('article', [
 <%- selectedPlugins.map(function (plugin) {
-    return '  ' + plugin.varName + '(' + (plugin.configValue || '') + ')';
+  return '  ' + plugin.varName + '(' + (plugin.configValue || '') + ')';
 }).join(',\n') %>
 ]);
-<% if (syntax) { %>
+
+<% if (syntax) { -%>
 // Prism syntax highlighting
 require('prismjs');
 require('prismjs/plugins/normalize-whitespace/prism-normalize-whitespace');
-<% } %>
+<% } -%>
