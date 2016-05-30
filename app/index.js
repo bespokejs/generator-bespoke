804458d2478ec87ddd4b8df226ca9598aee29788
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-05-30 17:40:52 +0200

    feat: add choice to use simple HTML (instead of Pug/Jade)

--- app/index.js
+++ app/index.js
@@ -35,11 +35,20 @@ var mandatoryPlugins = [
 
 var optionalPlugins = [];
 
+var PUGJS = 'Pug (formerly Jade)';
+
 var questions = [
   {
     name: 'title',
     message: 'What is the title of your presentation?',
     default: 'Hello World'
+  },
+  {
+    name: 'templatingLanguage',
+    message: 'Which templating language would you like to use?',
+    type: 'list',
+    choices: [PUGJS, 'HTML'],
+    default: PUGJS
   },
   {
     type: 'confirm',
@@ -98,6 +107,9 @@ module.exports = generators.Base.extend({
         this[usePluginName] = answers[plugin.name];
       }.bind(this));
 
+      this.usePug = (answers.templatingLanguage === PUGJS);
+      this.useHtml = (answers.templatingLanguage === 'HTML');
+
       this.syntax = answers.syntax;
       this.title = answers.title;
       this.useTheme = answers.useTheme;
@@ -109,7 +121,7 @@ module.exports = generators.Base.extend({
   configuring: function () {
 
     this.template('README.md', 'README.md');
-    this.copy('gulpfile.js', 'gulpfile.js');
+    this.template('gulpfile.js', 'gulpfile.js');
     this.copy('_gitignore', '.gitignore');
     this.copy('_editorconfig', '.editorconfig');
 
@@ -128,7 +140,6 @@ module.exports = generators.Base.extend({
       'gulp-connect': '^4.1.0',
       'gulp-csso': '^2.0.0',
       'gulp-plumber': '^1.1.0',
-      'gulp-pug': '^3.0.3',
       'gulp-rename': '^1.2.2',
       'gulp-stylus': '^2.5.0',
       'gulp-uglify': '^1.5.4',
@@ -136,6 +147,10 @@ module.exports = generators.Base.extend({
       'insert-css': '^0.2.0',
       'through': '^2.3.8'
     };
+
+    if (this.usePug) {
+      devDependencies['gulp-pug'] = '^3.0.3';
+    }
 
     this.selectedPlugins.forEach(function (plugin) {
       devDependencies['bespoke-' + plugin.name] = plugin.version;
@@ -156,7 +171,14 @@ module.exports = generators.Base.extend({
   },
 
   writing: function () {
-    this.template('src/index.pug', 'src/index.pug');
+
+    if (this.usePug) {
+      this.template('src/index.pug', 'src/index.pug');
+    }
+    if (this.useHtml) {
+      this.template('src/index.html', 'src/index.html');
+    }
+
     this.copy('src/images/bespoke-logo.jpg', 'src/images/bespoke-logo.jpg');
     this.template('src/scripts/main.js', 'src/scripts/main.js');
     this.template('src/styles/main.styl', 'src/styles/main.styl');
