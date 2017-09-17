eaf4cd77dcca2a20ec92d775cceb54ec10c64fb5
Dan Allen <dan.j.allen@gmail.com>
2017-09-17 00:43:05 -0600

    resolves #46 automatically populate repository field if detected

--- test/test-behaviour.js
+++ test/test-behaviour.js
@@ -4,7 +4,7 @@
 var path = require('path');
 var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
-var spawn = require('child_process').spawn;
+var childProcess = require('child_process');
 
 function createErrorFromStdErr(errorOutput) {
 
@@ -25,7 +25,7 @@ function createErrorFromStdErr(errorOutput) {
 // seems that using child_process.exec does not retrieve the whole stderr :-(
 function spawnAndCollectStderr(cmd, args, opts, done) {
 
-  var spawnedProcess = spawn(cmd, args, opts);
+  var spawnedProcess = childProcess.spawn(cmd, args, opts);
 
   var processErrors = '';
   spawnedProcess.stderr.on('data', function (errBuffer) {
@@ -50,6 +50,10 @@ describe('bespoke generator', function () {
     this.timeout(60000);
 
     this.ctx = helpers.run(path.join(__dirname, '../app'))
+      .inTmpDir(function (dir) {
+        childProcess.execSync('git init .'); 
+        childProcess.execSync('git remote add origin git@github.com:presenter/presentation-foobar.git'); 
+      })
       .withOptions({ skipInstall: false })
       .withPrompts({
         'title': 'Foobar Test Title',
@@ -66,13 +70,23 @@ describe('bespoke generator', function () {
     this.ctx.cleanTestDirectory();
   });
 
+  it('should populate the repository field in package.json if called within a git repository', function (done) {
+    assert.jsonFileContent('package.json', {
+      repository: {
+        type: 'git',
+        url: 'https://github.com/presenter/presentation-foobar.git'
+      }
+    });
+    return done(null);
+  });
+
   it('should provide a "gulp serve" command with a basic working bespoke deck', function (done) {
 
     // gulp serve + phantom tests can be long
     this.timeout(20000);
 
     // start gulp serve (on tmp dir by default)
-    var gulpServerProcess = spawn('./node_modules/.bin/gulp', ['serve']);
+    var gulpServerProcess = childProcess.spawn('./node_modules/.bin/gulp', ['serve']);
 
     // Wait for gulp serve to be ready (5 sec)
     setTimeout(function () {
